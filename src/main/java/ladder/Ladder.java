e46aa8547253216c4b77d1824770df314e645c3a
ro-el-c <chung3286@naver.com>
2023-03-21 02:11:21 +0900

    week1_Ladder - Ladder, Position, LadderTest

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -1,11 +1,61 @@
 package ladder;
 
 public class Ladder {
-
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
+        validateNum(row, numberOfPerson);
         rows = new int[row][numberOfPerson];
     }
 
+    public void drawLine(Position position) {
+        rows[position.getPointX()][position.getPointY()] = 1;
+        rows[position.getPointX()][position.getPointY()+1] = -1;
+    }
+
+    public int run(int ladderNum){
+        int row=0;
+
+        while(row<rows.length){
+            if(rows[row][ladderNum] == 1) {
+                row++;
+                ladderNum++;
+                continue;
+            }
+            if(rows[row][ladderNum] == -1){
+                row++;
+                ladderNum--;
+                continue;
+            }
+            row++;
+        }
+
+        return ladderNum;
+    }
+
+    private static void validateNum(int row, int numberOfPerson) {
+        // 사람 - 2명 이상
+        // 사다리 - (사람수+1)층 이상
+        //  -> 사다리 라인이 (사람수-1)개 이상이라고 "가정"
+        if (numberOfPerson < 2) {
+            System.out.println("사람이 2명 미만입니다.");
+            throw new IllegalArgumentException();
+        }
+        if (row < numberOfPerson + 1) {
+            System.out.println("사다리 층이 부족합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getNumberOfPerson(){
+        return rows[0].length;
+    }
+
+    public int getLadderLength(){
+        return rows.length;
+    }
+
+    public int getValue(int x, int y){
+        return rows[x][y];
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,6 +1,53 @@
 package ladder;
 
-public class LadderTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
+class LadderTest {
+    Ladder ladder;
+    Position position;
+
+    @Test
+    @DisplayName("사다리 만들기")
+    @BeforeEach
+    void makeLadder(){
+        ladder = new Ladder(6, 4);
+    }
+
+    @Test
+    @DisplayName("사다리 만들기 유효성 검사")
+    void checkLadder(){
+        assertThrows(IllegalArgumentException.class, () -> new Ladder(3,3));
+    }
+
+    @Test
+    @DisplayName("Position 유효성 검사")
+    void isPositionRight(){
+        assertThrows(IllegalArgumentException.class, () -> new Position(0,1, ladder));
+        new Position(1,1, ladder);
+    }
+
+    @Test
+    @DisplayName("사다리라인 만들기")
+    void drawLine(){
+        // position -> 사다리라인의 왼쪽 좌표
+        position = new Position(1, 1, ladder);
+        ladder.drawLine(position);
+
+        // 사다리라인 만들고 해당 좌표에 Position 생성 유효성 검사
+        assertThrows(IllegalArgumentException.class, () -> new Position(1,2, ladder));
+    }
+
+    @Test
+    @DisplayName("사다리타기")
+    void run(){
+        ladder.drawLine(new Position(1, 0, ladder));
+        ladder.drawLine(new Position(2, 2, ladder));
+        ladder.drawLine(new Position(4, 1, ladder));
+        assertEquals(1, ladder.run(3));
+    }
 }
