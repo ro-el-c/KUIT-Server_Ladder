09dc57280f7acc430d9ed48d2675153f59af8dd1
ro-el-c <chung3286@naver.com>
2023-03-28 01:45:03 +0900

    fix: Position 클래스 내 변수명 수정
    pointX -> positionRow / pointY -> positionCol

--- src/main/java/ladder/Position.java
+++ src/main/java/ladder/Position.java
@@ -5,11 +5,11 @@ public class Position {
     NaturalNumber y;
     // pointX의 범위
 
-    public Position(NaturalNumber pointX, NaturalNumber pointY, LadderCreator ladderCreator) {
-        validateX(pointX, ladderCreator);
-        this.x = new NaturalNumber(pointX.getValue());
-        validateY(pointY, ladderCreator);
-        this.y = new NaturalNumber(pointY.getValue());
+    public Position(NaturalNumber positionRow, NaturalNumber positionCol, LadderCreator ladderCreator) {
+        validateX(positionRow, ladderCreator);
+        this.x = new NaturalNumber(positionRow.getValue());
+        validateY(positionCol, ladderCreator);
+        this.y = new NaturalNumber(positionCol.getValue());
     }
 
     public NaturalNumber getX() {
@@ -21,12 +21,12 @@ public class Position {
     }
 
     // TODO: 적절한 validation 위치 더 고민해보기
-    private void validateX(NaturalNumber pointX, LadderCreator ladderCreator){
-        if(pointX.getValue() >= ladderCreator.getNumberOfFloors()-1)
+    private void validateX(NaturalNumber positionRow, LadderCreator ladderCreator){
+        if(positionRow.getValue() >= ladderCreator.getNumberOfFloors()-1)
             throw new IllegalArgumentException("사다리 층을 벗어났습니다.");
     }
-    private void validateY(NaturalNumber pointY, LadderCreator ladderCreator){
-        if(pointY.getValue() == 1 || pointY.getValue() > ladderCreator.getNumberOfPerson())
+    private void validateY(NaturalNumber positionCol, LadderCreator ladderCreator){
+        if(positionCol.getValue() == 1 || positionCol.getValue() > ladderCreator.getNumberOfPerson())
             throw new IllegalArgumentException("사다리 라인을 생성할 수 있는 위치가 아닙니다.");
     }
 }
