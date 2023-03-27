6097460061d1d320bdee8234580ebe77b9b15ab4
ro-el-c <chung3286@naver.com>
2023-03-28 01:15:07 +0900

    fix: PositionTest 내용 삭제

--- src/test/java/ladder/PositionTest.java
+++ src/test/java/ladder/PositionTest.java
@@ -1,15 +1,11 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PositionTest {
-    @Test
-    @DisplayName("Position 유효성 검사")
-    void isPositionRight(){
-        assertThrows(IllegalArgumentException.class, () -> new Position(0,1));
-        new Position(1,1);
-    }
+
 }
