cb757169b9caf31007e5fcc6fead7619de5c924b
ro-el-c <chung3286@naver.com>
2023-03-28 01:59:53 +0900

    test: update LadderGameTest DisplayName

--- src/test/java/ladder/LadderGameTest.java
+++ src/test/java/ladder/LadderGameTest.java
@@ -1,5 +1,6 @@
 package ladder;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,19 +22,7 @@ class LadderGameTest {
     }
 
     @Test
-    @DisplayName("사다리라인 만들기 - drawLine")
-    void drawLine(){
-        NaturalNumber positionRow = new NaturalNumber(1);
-        NaturalNumber positionCol = new NaturalNumber(2);
-        Position position = new Position(positionRow, positionCol, this.ladderCreator);
-        ladderGame.drawLine(position);
-
-        // 이미 사다리 라인이 존재할 때, 생성할 수 없음
-        assertThrows(IllegalArgumentException.class, () -> ladderGame.drawLine(position));
-    }
-
-    @Test
-    @DisplayName("사다리타기 - run")
+    @DisplayName("사다리타기 게임 실행")
     void run(){
         ladderGame.drawLine(new Position(new NaturalNumber(1), new NaturalNumber(3), this.ladderCreator));
         ladderGame.drawLine(new Position(new NaturalNumber(2), new NaturalNumber(4), this.ladderCreator));
