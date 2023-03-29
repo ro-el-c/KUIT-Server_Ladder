861f54cf092f10fd5b66461f17e70a880ab92883
ro-el-c <chung3286@naver.com>
2023-03-29 16:01:15 +0900

    feat: create RandomLadderCreator
    규격만 입력하면 사다리 라인을 자동으로 생성

---
+++ src/main/java/ladder/creator/RandomLadderCreator.java
@@ -0,0 +1,62 @@
+package ladder.creator;
+
+import ladder.LadderSize;
+import ladder.NaturalNumber;
+import ladder.Position;
+import ladder.Row;
+
+import static ladder.NaturalNumber.createNaturalNumber;
+
+public class RandomLadderCreator implements LadderCreator{
+    // 사용자가 숫자를 지정하진 않지만, 결국 난수가 정해지면 CustomLadderCreator 사다리 생성
+
+    private final LadderSize ladderSize;
+    private final CustomLadderCreator ladderCreator;
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+    public RandomLadderCreator(CustomLadderCreator ladderCreator) {
+        this.ladderCreator = ladderCreator;
+        this.ladderSize = ladderCreator.getLadderSize();
+        drawLines();
+    }
+
+    private void drawLines(){
+        int cnt=0;
+        // TODO: 제약 조건에 의한 하드코딩 해결
+        while(cnt < totalLineNumber()){
+            NaturalNumber row = createNaturalNumber(randomNumberGenerator.getRandomNumber(ladderCreator.getNumberOfFloors()-2));
+            NaturalNumber col = createNaturalNumber(randomNumberGenerator.getRandomNumber(ladderCreator.getNumberOfPerson()-1));
+            Position position = new Position(row, col, this.ladderCreator);
+
+            try{
+                ladderCreator.drawLine(position);
+                cnt++;
+            } catch (IllegalArgumentException e){
+            }
+        }
+    }
+
+    private int totalLineNumber(){
+        return (int)(ladderSize.getLadderSize() * 0.3);
+    }
+
+    @Override
+    public void drawLine(Position position) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Row[] getRows() {
+        return ladderCreator.getRows();
+    }
+
+    @Override
+    public int getNumberOfFloors() {
+        return ladderCreator.getNumberOfFloors();
+    }
+
+    @Override
+    public int getNumberOfPerson() {
+        return ladderCreator.getNumberOfPerson();
+    }
+}
